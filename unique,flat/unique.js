5f77df522e8178f7b29729f4619b4b3ce61ffdfd
ruoruoji <30526662+ruoruoji@users.noreply.github.com>
2019-05-19 20:14:19 +0800

    Update unique.js

--- unique,flat/unique.js
+++ unique,flat/unique.js
@@ -1,10 +1,35 @@
-var a = [1,2,3,1,2,3,3,1,4,5];
+var a = [1,2,3,4,5,3,3,3,3,3,3,3,3,3,4,5,5,5,5,55,5,5,5,5,5,6,'3']
+console.time(1)
+console.log( [...new Set(a)] )
+console.timeEnd(1)
 
-console.log( [...new Set(a)] );     //first method
-
-Array.prototype.myUnique = function() {  //second method
+Array.prototype.myUnique = function() {  
     return this.reduce((res, item) => {
         return res.indexOf(item) === -1? [...res, item] : res;
     }, [])
 }
+console.time(2)
 console.log( a.myUnique() );
+console.timeEnd(2)
+
+Array.prototype._myUnique = function() {  
+    var arr = []
+    for (let item of this) {
+        arr = arr.indexOf(item) === -1 ? arr.concat(item) : arr
+    }
+    return arr
+}
+console.time(3)
+console.log( a._myUnique() );
+console.timeEnd(3)
+
+Array.prototype.__myUnique = function() {  
+    var len = this.length
+    for (let i = len-1; i >= 0; i--) {
+        if (this.indexOf(this[i]) !== i) this.splice(i, 1)
+    }
+    return this
+}
+console.time(4)
+console.log( a.__myUnique() );
+console.timeEnd(4)
