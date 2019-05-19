9ee304142f4014b506c4f3313b38705fd28b390b
ruoruoji <30526662+ruoruoji@users.noreply.github.com>
2019-05-19 20:15:51 +0800

    Update unique.js

--- unique,flat/unique.js
+++ unique,flat/unique.js
@@ -1,4 +1,4 @@
-var a = [1,2,3,4,5,3,3,3,3,3,3,3,3,3,4,5,5,5,5,55,5,5,5,5,5,6,'3']
+var a = [1,2,3,4,5,3,3,3,3,3,3,3,3,3,4,5,5,5,5,55,5,5,5,5,5,6,'3']  //indexOf强相等，能判别出字符串
 console.time(1)
 console.log( [...new Set(a)] )
 console.timeEnd(1)
@@ -23,7 +23,7 @@ console.time(3)
 console.log( a._myUnique() );
 console.timeEnd(3)
 
-Array.prototype.__myUnique = function() {  
+Array.prototype.__myUnique = function() {            //没有另外开辟出空间，节约了内存
     var len = this.length
     for (let i = len-1; i >= 0; i--) {
         if (this.indexOf(this[i]) !== i) this.splice(i, 1)
