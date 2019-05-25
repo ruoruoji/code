cac4bcece8d9afbc88609f91817248fbbe286131
ruoruoji <30526662+ruoruoji@users.noreply.github.com>
2019-05-25 13:04:48 +0800

    Update list_Sort.js

--- sort/list_Sort.js
+++ sort/list_Sort.js
@@ -12,3 +12,29 @@ function ListNode(val) {
  c.next = d
  d.next = e 
 
+var sortList = function(head, tail = null) {
+    var partitionP
+    if (head === tail) return head
+    partitionP = partition(head, tail)
+    sortList(head, partitionP)
+    if(partitionP.next) sortList(partitionP.next, tail)
+    return head
+};
+
+var partition = function(head, tail) {
+    var referP = head
+    var indexP = referP.next
+    var partitionP = head
+    for (let traverP = indexP; traverP !== tail; ) {
+        if (traverP.val < referP.val) {
+            if (indexP !== traverP) [indexP.val, traverP.val] = [traverP.val, indexP.val]
+            indexP = indexP.next
+        }
+        traverP = traverP.next
+    }
+    while (partitionP.next !== indexP) {
+        partitionP = partitionP.next
+    }
+    [head.val, partitionP.val] = [partitionP.val, head.val]
+    return partitionP
+}
