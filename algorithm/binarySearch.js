52d24324a1722063ccb8d444ad6a330f1c7a90cb
ruoruoji <30526662+ruoruoji@users.noreply.github.com>
2019-06-01 10:40:53 +0800

    Update binarySearch.js

--- algorithm/binarySearch.js
+++ algorithm/binarySearch.js
@@ -1,12 +1,28 @@
 var arr = [1,3,5,7,9,10]
 
-function binarySearch(left, right, item, arr) {
-    if (left === right) {
-        return item === arr[left] ? left : false
-    }
-    var mid = Math.ceil((left + right) / 2)
-    if (item < arr[mid]) return search(left, mid - 1, item, arr)
-    return search(mid, right, item, arr)
-}
+// function searchIndex(left, right, item, arr) {        //递归实现
+//     if (left > right) return -1
+//     var mid = Math.floor((left + right) / 2 )
+//     if (arr[mid] === item) return mid
+//     if (item < arr[mid]) return searchIndex(left, mid - 1, item, arr)
+//     return searchIndex(mid + 1, right, item, arr)
+// }
+// function binarySearch(arr, item) {
+//     return searchIndex(0, arr.length - 1, item, arr)
+// }
 
-console.log(binarySearch(0, arr.length - 1, 5, arr)) 
+//-------------------------------------------------------------------------------------------------
+
+function searchIndex(left, right, item, arr) {       //非递归实现
+    while (left <= right) {
+        var mid = Math.floor((left + right) / 2)
+        if (arr[mid] === item) return mid
+        if (arr[mid] < item) left = mid + 1
+        else right = mid - 1
+    }
+    return -1
+}
+function binarySearch(arr, item) {
+    return searchIndex(0, arr.length - 1, item, arr)
+}
+console.log(binarySearch(arr, 5))  
