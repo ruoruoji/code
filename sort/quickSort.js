0f76cd2beb56a7e7fc58d09ab2963adf1da4c597
ruoruoji <30526662+ruoruoji@users.noreply.github.com>
2019-09-09 21:03:48 +0800

    Update quickSort.js

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,56 +1,38 @@
-var arr = [45, 10, 11, 88, 34, 90]
-// Array.prototype.quickSort = function(flag, i) {
-//     var arr = this
+const arr = [45, 10, 11, 88, 34, 90]
+
+// const quickSort = arr => {
 //     if (arr.length < 2) return arr
-//     var splitItem = arr[Math.floor(arr.length/2)]         //不抽离该元素会死循环
-//     var [leftArray, rightArray] = [[], []]
-//     for (let item of arr) {
-//         if (item < splitItem) leftArray.push(item)
-//         else rightArray.push(item)
-//     }
-//     return leftArray.quickSort("left", i+1).concat(rightArray.quickSort("right", i+1))
+//     const spliceItem = arr.splice(0, 1)
+//     const leftArr = []
+//     const rightArr = []
+//     arr.forEach(item => {
+//         if (item >= spliceItem) rightArr.push(item)
+//         else leftArr.push(item)
+//     });
+//     return leftArr.concat(spliceItem, rightArr);
 // }
-// console.log(arr.quickSort("first", 1))
 
-//-----------------------------------------------------------------------------------------------------
 
-// Array.prototype.quickSort = function(flag, i) {
-//     var arr = this
-//     if (arr.length < 2) return arr
-//     var splitItem = arr.splice(0, 1)                           //基准值元素的选择影响效率
-//     var [leftArray, rightArray] = [[], []]
-//     for (let item of arr) {
-//         if (item < splitItem) leftArray.push(item)
-//         else rightArray.push(item)
-//     }
-//     return leftArray.quickSort("left", i+1).concat(splitItem, rightArray.quickSort("right", i+1))
-// }
-// console.log(arr.quickSort("first", 1))
-           //以上两方法的i仅为了测试错误
-//-----------------------------------------------------------------------------------------------------
-
-function quickSort(arr, left, right) {        //减少内存的快排
-    var len = arr.length
-    var left = typeof left === "number" ? left : 0
-    var right = typeof right === "number" ? right : len - 1
-    var partitionIndex
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    const pivot = arr[Math.ceil((left + right)/ 2 )]   //0为基准值
+    let leftPoint = left
+    let rightPoint = right
+    let index
     if (left < right) {
-        partitionIndex = partition(arr, left, right)
-        quickSort(arr, left, partitionIndex - 1)
-        quickSort(arr, partitionIndex + 1, right)
-    }
-    return arr  
-}
-function partition(arr, left, right) {
-    var pIndex = left
-    var index = pIndex + 1
-    for (let i = index; i <= right; i++) {
-        if (arr[pIndex] > arr[i]) {
-            [arr[i], arr[index]] = [arr[index], arr[i]]
-            index++
+        while (leftPoint < rightPoint) {
+            while(arr[rightPoint] > pivot) {
+                rightPoint--
+            }
+            while(arr[leftPoint] < pivot) {
+                leftPoint++
+            }
+            [arr[leftPoint], arr[rightPoint]] = [arr[rightPoint], arr[leftPoint]]
         }
+        index = leftPoint
+        quickSort(arr, left, index - 1)
+        quickSort(arr, index + 1, right)
     }
-    [arr[pIndex], arr[index - 1]] = [arr[index - 1], arr[pIndex]]
-    return index - 1
+    return arr
 }
+
 console.log(quickSort(arr))
